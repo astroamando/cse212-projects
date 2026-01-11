9e8a9f9f70c6065f4fcc6959f0f4c1eb09d863d9
astroamando <amando.filho83@gmail.com>
2026-01-10 19:54:21 -0700

    First attempt - could not get results from Array_Tests.cs.

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -6,6 +6,12 @@ public static class Arrays
     /// integer greater than 0.
     /// </summary>
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
+    // Plan:
+    // 1. Create a new double[] with lenght as count
+    // 2. For i from 0 to count-1:
+    // the i multiple is number times i + 1
+    // store it in the array
+    // 3. return the array
     public static double[] MultiplesOf(double number, int length)
     {
         // TODO Problem 1 Start
@@ -13,7 +19,14 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        double[] r = new double[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            r[i] = number * (i + 1);
+        }
+
+        return r; // replace this return statement with your own
     }
 
     /// <summary>
@@ -23,11 +36,30 @@ public static class Arrays
     ///
     /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
     /// </summary>
+    // 1. rotating right by amount items move to the front
+    // 2. Compute index as the count minus the amount rotated
+    // 3. slice the list into two parts
+    // left = data[0 to Index split]
+    // right = data[index split to end]
+    // 4. Clear original list and add right and left back in
     public static void RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        int n = data.Count;
+
+        amount = amount % n;
+
+        int indexSplit = n - amount;
+
+        List<int> left = data.GetRange(0, indexSplit);
+        List<int> right = data.GetRange(indexSplit, n- indexSplit);
+
+        data.Clear();
+        data.AddRange(right);
+        data.AddRange(left);
     }
 }
