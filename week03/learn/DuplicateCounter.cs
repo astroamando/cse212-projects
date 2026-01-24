56681418072e6862781430622f9a90b27c4303bb
astroamando <amando.filho83@gmail.com>
2026-01-23 19:27:08 -0700

    First problem.

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -24,7 +24,18 @@
 
     private static int CountDuplicates(int[] data)
     {
-        // Add code here.
-        return 0;
+        HashSet<int> values = new HashSet<int>();
+
+        int duplicates = 0;
+
+        foreach (int value in data)
+        {
+            if (!values.Add(value))
+            {
+                duplicates++;
+            }
+        }
+
+        return duplicates;
     }
 }
